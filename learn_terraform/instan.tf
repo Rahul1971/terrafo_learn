307c9012656486d4d53e52723bc0a0aecf6ca846
Rahul1971 <30292055+Rahul1971@users.noreply.github.com>
2025-03-10 20:07:22 +0530

    Initial commit

---
+++ learn_terraform/instan.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region=us-east-1
+}
+resource "aws_instance" "testserver" {
+  ami="ami-0c7af5fe939f2677f"
+  instance_type="t2.micro"
+
+  tags = {
+    Name=testserver
+  }
+}
