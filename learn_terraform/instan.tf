ef6410f9b11283992fa6aff03d52f105c981472e
Rahul1971 <rahulgandham1997@gmail.com>
2025-03-10 20:50:12 +0530

    Initial commit

--- learn_terraform/instan.tf
+++ learn_terraform/instan.tf
@@ -1,5 +1,24 @@
 provider "aws" {
   region="us-east-1"
+
+}
+
+resource "aws_security_group" "ssh_access" {
+  name_prefix = "allow_ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Use your IP CIDR for better security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 resource "aws_instance" "testserver" {
   ami="ami-0c7af5fe939f2677f"
