2ce641602178861f8e6b3fd38e0d54bafe6f15a2
Rahul1971 <rahulgandham1997@gmail.com>
2025-04-24 20:11:52 +0530

    Initial commit

--- learn_terraform/instan.tf
+++ learn_terraform/instan.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 provider "aws" {
   region="us-east-1"
 
@@ -20,9 +28,16 @@ resource "aws_security_group" "ssh_access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_key_pair" "example" {
+  key_name   = "all" # Change this to your desired key name# Path to the public key file (typically generated with ssh-keygen)
+  public_key = ""
+}
+
+
 resource "aws_instance" "testserve2" {
   ami="ami-0c7af5fe939f2677f"
   instance_type="t2.micro"
+  
 
   tags = {
     Name="testser2"
